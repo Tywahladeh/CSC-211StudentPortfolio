c5ba9cbcf5a6b8ba50e63b942d904f7a097e57d9
AyoolaFaith <160761233+AyoolaFaith@users.noreply.github.com>
2024-02-29 15:06:52 +0100

    Add files via upload

---
+++ PHP/Conditional.php.php
@@ -0,0 +1,31 @@
+<?php
+$level1= 100;
+$level2= 200;
+$level3= 300;
+$level4= 400;
+
+//student's level
+$level=200;
+
+//if statement
+if($level==$level1){
+    echo "Student is in 100 level";
+}
+
+//if else statement
+if($level==$level1){
+    echo "Student is in 100 level";
+}
+else
+echo "the student is not in level 1 <br>";
+
+//if elseif statement
+if($level==$level3){
+    echo "Student is in level 2";
+}
+elseif($level==$level2){
+    echo "This is the level";
+}
+else
+echo "Stop typing rubbish";
+?>
---
+++ PHP/firstphpprogram.php
@@ -0,0 +1,27 @@
+<?php
+/*There are several functions that can be performed in php*/ 
+//Displaying contents
+Print("Hello Class <br>");
+echo"You are welcome <br>";
+$school="Crawford University";
+echo $school ."<br>";
+?>
+<?php
+$num1=15;
+$num2=5;
+//Arithemetic Operations
+$Addition=$num1+$num2;
+echo $Addition ."<br>";
+//Subtraction
+$Subraction= $num1-$num2;
+echo $Subraction ."<br>";
+//Division
+$Division= $num1/$num2;
+echo $Division ."<br>";
+//Multiply
+$Multiply= $num1*$num2;
+echo $Multiply ."<br>";
+//Modulus
+$Modulus= $num1%$num2;
+echo $Modulus ."<br>";
+?>
