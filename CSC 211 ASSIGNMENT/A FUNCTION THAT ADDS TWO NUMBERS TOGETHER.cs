cd36d684049b5788744bdab74dba400da39a7647
DAMOLA-11 <162458374+DAMOLA-11@users.noreply.github.com>
2024-03-06 05:43:01 -0800

    Add files via upload

---
+++ CSC 211 ASSIGNMENT/A FUNCTION THAT ADDS TWO NUMBERS TOGETHER.cs
@@ -0,0 +1,11 @@
+using system;
+namespace program{
+    class myprogram{
+        static int sum(inta,intb){
+            returna+b:
+                }
+        static void main(strings{}args){
+            console.writeline(sum(5,6))
+        }
+    }
+}
---
+++ CSC 211 ASSIGNMENT/create website/conditionalstatement.php
@@ -0,0 +1,36 @@
+<?php
+//levels//
+$level1=100;
+$level2=200;
+$level3=300;
+$level4=400;
+
+//students level
+$level1=200;
+
+//if statement
+if($level1==$level1){
+    echo "student is in 100 level";
+}
+
+
+//if else statement
+if($level1==$level1){
+    echo "student is in 100 level";
+}
+else
+echo "the student is not in level 1";
+
+
+
+//if elseif statement
+if($level1==$level1){
+    echo "student is in 100 level";
+}
+
+if($level1==$level2){
+    echo="student is in 200level";
+}
+
+if($level1==$level2)
+?>
